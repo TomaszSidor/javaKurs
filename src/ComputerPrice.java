9d142606fa9101ccdceac9c57566a6516586197b
tomsi <tom.sidor@gmail.com>
2019-03-02 09:26:26 +0100

    drugi, kilka zadań po pierwszym weekendzie

--- src/ComputerPrice.java
+++ src/ComputerPrice.java
@@ -2,52 +2,47 @@
  * W klasie ComputerPrice wydziel metody getComputerPrice(), getMonitorPrice() i getComputerAndMonitorPrice(),
  * ostatnia z metod ma korzystać z dwóch pierwszych. Zmienną VAT ustaw jako pole klasy ComputerPrice.
  */
-
-
 public class ComputerPrice {
     double vatTax = 1.23;
-    double plyta_glowna = 265;
-    double procesor = 1000;
+    double mainBoard = 265;
+    double procesor= 1000;
     double ram = 800;
-    double dysk_twardy = 400;
+    double HDD= 400;
     double monitor = 500;
 
-    public ComputerPrice(double vatTax, double plyta_glowna, double procesor,
-                         double ram, double dysk_twardy, double monitor) {
+    public ComputerPrice(){};
+
+    public ComputerPrice(double vatTax, double mainBoard, double procesor,
+                         double ram, double HDD, double monitor) {
         this.vatTax = vatTax;
-        this.plyta_glowna = plyta_glowna;
+        this.mainBoard = mainBoard;
         this.procesor = procesor;
         this.ram = ram;
-        this.dysk_twardy = dysk_twardy;
+        this.HDD = HDD;
         this.monitor = monitor;
     }
-
     public double getMonitorPrice(){
         return this.monitor * this.vatTax;
     }
-    public double getComputerPrice(){
 
-        return (this.plyta_glowna + this.procesor + this.ram + this.dysk_twardy) * this.vatTax;
+    public double getComputerPrice(){
+        return (this.mainBoard + this.procesor + this.ram + this.HDD) * this.vatTax;
     }
 
     public double getComputerAndMonitorPrice(){
-
         return getMonitorPrice() + getComputerPrice();
-
-
     }
 
     public static void main(String[] args) {
-        ComputerPrice newComputer = new ComputerPrice(1.23, 583,729,
+        ComputerPrice newComputer = new ComputerPrice(1.23, 522,729,
                                                     399,600, 1000);
+
+        ComputerPrice newComputer2 = new ComputerPrice();
+
+        System.out.println("Computer2  = " + newComputer2.getComputerAndMonitorPrice());
         System.out.println("Computer price = " +  newComputer.getComputerPrice());
         System.out.println("Monitor price = " + newComputer.getMonitorPrice());
-        System.out.println("Computer and MOnitor price = " + newComputer.getComputerAndMonitorPrice());
-
-
-
+        System.out.println("Computer and Monitor price = " + newComputer.getComputerAndMonitorPrice());
     }
-
-
 }
 
